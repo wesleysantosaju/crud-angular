06a6e5d42b0c5d48e07f0be99d3fab4d0946c8f8
Wesley Santos <wvsantos15@gmail.com>
2023-07-18 20:17:42 -0300

    alterado o Cursos Service para fazer uma chamada HTTP Get usando o HttpClient do Angular com RXJS.

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,15 +1,17 @@
-import { Component } from '@angular/core';
+import { CoursesService } from './../services/courses.service';
+import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
-import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService){
