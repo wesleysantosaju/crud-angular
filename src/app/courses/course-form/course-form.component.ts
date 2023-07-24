3c86ba449ed42e41a668ef3aa82ac91b1f884189
Wesley Santos <wvsantos15@gmail.com>
2023-07-24 12:58:29 -0300

    salvar os dados do formulário para criar um registro novo, fazer a chamada HTTP Post utilizando o HttpClient e a classe de serviço, além de tratar erros e mostrar uma notificação para o usuário em caso de exceção no Angular.

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CoursesService } from './../services/courses.service';
 
 
 @Component({
@@ -11,7 +13,9 @@ export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder){
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar){
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -19,7 +23,12 @@ export class CourseFormComponent implements OnInit {
   }
 
   onSubmit(){
-
+    this.service.save(this.form.value).subscribe({
+      next: (data) => console.log(data),
+          error: () => {
+            this.onError();
+      },
+    });
   }
 
   onCancel(){
@@ -28,5 +37,9 @@ export class CourseFormComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  private onError(){
+    this._snackBar.open("Erro ao salvar curso.", "", {duration: 5000});
   }
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -20,4 +20,8 @@ export class CoursesService {
       );
 
   }
+  save(record: Course){
+    return this.httpClient.post<Course>(this.API, record);
+
+  }
 }
